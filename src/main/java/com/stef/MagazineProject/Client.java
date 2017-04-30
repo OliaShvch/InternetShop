d2deb37e16df4af1e0cf4d3f958bd5e62ad74f34
OliaShvch <kardasholga0@gmail.com>
2017-04-30 19:03:10 +0300

    30.04

--- src/main/java/com/stef/MagazineProject/Client.java
+++ src/main/java/com/stef/MagazineProject/Client.java
@@ -3,12 +3,14 @@ package com.stef.MagazineProject;
 import java.util.GregorianCalendar;
 
 public class Client extends Human {
+    private FavouriteList list;
     private static int nextId = 1;
     private int id;
     private String phoneNumber;
     private String address;
 
     public Client() {
+        list=new FavouriteList();
         setName("name");
         setSurname("surname");
         setBornDate(new GregorianCalendar(1990, 01, 01));
@@ -20,6 +22,7 @@ public class Client extends Human {
     public Client(String name, String surname,
                   int year, int month, int day,
                   String phoneNumber, String address) {
+        list=new FavouriteList();
         setName(name);
         setSurname(surname);
         setBornDate(new GregorianCalendar(year, month, day));
@@ -49,12 +52,21 @@ public class Client extends Human {
         nextId++;
     }
 
+    public void addProductToFavouriteList(){
+        list.addProduct();
+    }
+
+    public void deleteProductFromFavouriteList(){
+        list.deleteProduct();
+    }
+
     @Override
     public String toString() {
         return "Client{" +
-                "id=" + id +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", address='" + address + '\'' +
+                "id: " + id +
+                ", phoneNumber: " + phoneNumber +
+                ", address: " + address +
+                ", list of favourite products  " + list.toString()+
                 '}';
     }
 }
